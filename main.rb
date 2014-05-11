19a80da5024006f4b7655f6ea0157c284d8315a9
erikareyes695 <erika.reyes95@gmail.com>
2014-05-12 02:54:11 +0800

    add

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require './boot.rb'
+require './money_calculator.rb'
 
 # ROUTES FOR ADMIN SECTION
 get '/admin' do
@@ -42,4 +43,53 @@ get '/delete_product/:id' do
   @product.destroy!
   redirect to '/admin'
 end
+
 # ROUTES FOR ADMIN SECTION
+
+# ROUTES FOR CUSTOMER
+
+get '/' do
+  @product = Item.all
+  @random_product = @product.sample(10)
+	erb :index
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/products' do
+  @product = Item.all
+	erb :products
+end
+
+get '/buy_product/:id' do
+  @product = Item.find(params[:id])
+  erb :buy_form
+end
+
+post '/buy_product/:id' do
+	@product = Item.find(params[:id]) #hahanapin yung item
+	@quantity = params[:quantity].to_i #ilan yung binili
+	@cost = @product.price.to_i #price of product
+	#lalagay lahat ng pera ng user sa isang variable
+	@total_money = MoneyCalculator.new(params[:ones],params[:fives],params[:tens],params[:twenties],params[:fifties],params[:hundreds],params[:five_hundreds],params[:thousands])
+	#compute for change
+	@change_total = @total_money.change(@cost, @quantity)
+	#conditions
+	if @total_money.total.to_i >= (@cost * @quantity) #kapag mas malaki yung pera kaysa sa dapat babayaran
+		if @product.quantity.to_i >= @quantity #kapag may laman pa
+			@product.update_attributes!( #update inventory
+        	quantity: @product.quantity - @quantity,
+        	sold: @product.sold + @quantity)
+        	
+        erb :success
+        else 
+        	erb :fail
+        end
+	else
+		erb :fail
+	end
+end
+
+# ROUTES FOR CUSTOMER
--- money_calculator.rb
+++ money_calculator.rb
@@ -1,12 +1,80 @@
 class MoneyCalculator
 
-  def initialize(ones, fives, tens, twenties, fifties, hundreds, five_hundreds, thousands)
+	attr_accessor :total_c, :total
+	def initialize(ones, fives, tens, twenties, fifties, hundreds, five_hundreds, thousands)
     # each parameter represents the quantity per denomination of money
     # these parameters can be assigned to instance variables and used for computation
+    # convert to integer
+    @ones = ones.to_i * 1
+    @fives = fives.to_i * 5
+    @tens = tens.to_i * 10
+    @twenties = twenties.to_i * 20
+    @fifties = fifties.to_i  * 50
+    @hundreds = hundreds.to_i * 100
+    @five_hundreds = five_hundreds.to_i * 500
+    @thousands = thousands.to_i * 1000
+    
+    @total = @ones + @fives + @tens + @twenties + @fifties + @hundreds + @five_hundreds + @thousands
+    
+end
 
-    # add a method called 'change' that takes in a parameter of how much an item costs
-    # and returns a hash of how much change is to be given
-    # the hash will use the denominations as keys and the amount per denomination as values
-    # i.e. {:fives => 1, fifties => 1, :hundreds => 3}
+# add a method called 'change' that takes in a parameter of how much an item costs
+
+def change (cost, quantity) 
+	@total_change = @total - (cost.to_i * quantity.to_i) #subtract cost*quantity to the total money
+	@new_total = @total_change 
+	#looking for number of bills per denomination
+	if @total_change >= 1000
+ 		thousands_change = @total_change / 1000
+ 		@total_change = @total_change % 1000
+ 	end
+ 	
+ 	if @total_change >= 500
+ 		five_hundreds_change = @total_change / 500
+ 		@total_change = @total_change % 500
+ 	end
+ 	
+ 	if @total_change >= 100
+ 		hundreds_change = @total_change / 100
+ 		@total_change = @total_change % 100
+ 	end
+ 	
+ 	if @total_change >= 50
+ 		fifties_change = @total_change / 50
+ 		@total_change = @total_change % 50
+ 	end
+ 	
+ 	if @total_change >= 20
+ 		twenties_change = @total_change / 20
+ 		@total_change = @total_change % 20
+ 	end
+ 	
+ 	if @total_change >= 10
+ 		tens_change = @total_change / 10
+ 		@total_change = @total_change % 10
+ 	end
+ 	
+ 	if @total_change >= 5
+ 		fives_change = @total_change / 5
+ 		@total_change = @total_change % 5
+ 	end
+ 	
+ 	if @total_change >= 1
+ 		ones_change = @total_change / 1
+ 		@total_change = @total_change % 1
+ 	end
+ 	
+ 	change = {
+ 		:ones => ones_change.to_i,
+ 		:fives => fives_change.to_i,
+ 		:tens => tens_change.to_i,
+ 		:twenties => twenties_change.to_i,
+ 		:fifties => fifties_change.to_i,
+ 		:hundreds => hundreds_change.to_i,
+ 		:five_hundreds => five_hundreds_change.to_i,
+ 		:thousands => thousands_change.to_i
+ 	}
+ 	
+ 	"<br /> #{change[:ones]} Php 1" "<br />#{change[:fives]} Php 5" "<br />#{change[:tens]} Php 10," "<br />#{change[:twenties]} Php 20" "<br />#{change[:fifties]} Php 50" "<br />#{change[:hundreds]} Php 100" "<br />#{change[:five_hundreds]} Php 500" "<br />#{change[:thousands]} Php 1000"
   end
 end
